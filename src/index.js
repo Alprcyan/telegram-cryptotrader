6179438e2e649da362bf97c9748fca610ac5b9bf
Alprcyan <alprcyan@gmail.com>
2018-01-23 00:08:11 +0800

    I'm pushing!

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
-import { resolve } from "dns";
-import { reject } from "../../../Library/Caches/typescript/2.6/node_modules/@types/bluebird";
-
 const Botgram = require("botgram");
 const config = require("./config");
 
 if (!config.botToken) {
-    console.error("No bot token specified. Please set BOT_TOKEN in env");
-    process.exit(1);
+  console.error("No bot token specified. Please set BOT_TOKEN in env");
+  process.exit(1);
 }
 
 const bot = new Botgram(config.botToken);
 
-bot.command("price", require("./commands/price"));
-bot.command("providers", require("./commands/providers"));
-bot.command("eth", require("./commands/eth"));
-bot.command("help", "start", require("./commands/help"));
+// bot.command("price", require("./commands/price"));
+// bot.command("providers", require("./commands/providers"));
+// bot.command("eth", require("./commands/eth"));
+// bot.command("help", "start", require("./commands/help"));
 
-// var foo = (path) => {
-//     return (msg, reply) => {
-//         var prom = new Promise((resolve, reject) => {
-//             require(path);
-//         });
 
-//         prom.catch((err) => {
-//             console.log(err);
-//         });
+var foo = (path) => {
+  return (msg, reply) => {
+    var bar = require(path);
+    var prom = new Promise(resolve, reject);
 
-//         return prom;
-//     }
-// }
+    prom.catch((err) => {
+      console.log(err);
+    });
 
-// bot.command("price", foo("./commands/price"));
-// bot.command("providers", foo("./commands/providers"));
-// bot.command("eth", foo("./commands/eth"));
-// bot.command("help", "start", foo("./commands/help"));
+    return prom;
+  }
+}
+
+bot.command("price", foo("./commands/price"));
+bot.command("providers", foo("./commands/providers"));
+bot.command("eth", foo("./commands/eth"));
+bot.command("help", "start", foo("./commands/help"));
