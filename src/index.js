a50a1a94da9516e6b54fde52f5470df3c9c4271d
Alprcyan <alprcyan@gmail.com>
2018-01-22 19:04:30 +0800

    I'm Pushing!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { resolve } from "dns";
+import { reject } from "../../../Library/Caches/typescript/2.6/node_modules/@types/bluebird";
+
 const Botgram = require("botgram");
 const config = require("./config");
 
@@ -8,7 +11,25 @@ if (!config.botToken) {
 
 const bot = new Botgram(config.botToken);
 
-bot.command("price", require("./commands/price"));
-bot.command("providers", require("./commands/providers"));
-bot.command("eth", require("./commands/eth"));
-bot.command("help", "start", require("./commands/help"));
+// bot.command("price", require("./commands/price"));
+// bot.command("providers", require("./commands/providers"));
+// bot.command("eth", require("./commands/eth"));
+// bot.command("help", "start", require("./commands/help"));
+
+var foo = (path) => {
+    return (msg, reply) => {
+        var bar = require(path);
+        var prom = new Promise(resolve, reject);
+
+        prom.catch((err) => {
+            console.log(err);
+        });
+
+        return prom;
+    }
+}
+
+bot.command("price", foo("./commands/price"));
+bot.command("providers", foo("./commands/providers"));
+bot.command("eth", foo("./commands/eth"));
+bot.command("help", "start", foo("./commands/help"));
