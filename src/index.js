f245c009d347379a30e962da8d870bcfc858e021
Alprcyan <alprcyan@gmail.com>
2018-01-23 00:09:55 +0800

    I'm pushing!

--- src/index.js
+++ src/index.js
@@ -8,26 +8,7 @@ if (!config.botToken) {
 
 const bot = new Botgram(config.botToken);
 
-// bot.command("price", require("./commands/price"));
-// bot.command("providers", require("./commands/providers"));
-// bot.command("eth", require("./commands/eth"));
-// bot.command("help", "start", require("./commands/help"));
-
-
-var foo = (path) => {
-  return (msg, reply) => {
-    var bar = require(path);
-    var prom = new Promise(resolve, reject);
-
-    prom.catch((err) => {
-      console.log(err);
-    });
-
-    return prom;
-  }
-}
-
-bot.command("price", foo("./commands/price"));
-bot.command("providers", foo("./commands/providers"));
-bot.command("eth", foo("./commands/eth"));
-bot.command("help", "start", foo("./commands/help"));
+bot.command("price", require("./commands/price"));
+bot.command("providers", require("./commands/providers"));
+bot.command("eth", require("./commands/eth"));
+bot.command("help", "start", require("./commands/help"));
